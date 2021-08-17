c64bd59715286a0789bba27e993441d54a228e12
cindyeke <officialcee1@gmail.com>
2021-08-17 09:19:49 +0100

    complete flip functionality on click of page direction icon

--- src/index.js
+++ src/index.js
@@ -5,28 +5,88 @@ import {
   handleEventMove,
 } from "./drag-to-scroll";
 
+// Drag To Scroll Functionality
 const projectNavContainer = document.querySelector(
   "[data-project-nav-container]"
 );
+
+if (projectNavContainer) {
+  projectNavContainer.addEventListener("touchstart", handleEventStart);
+  projectNavContainer.addEventListener("touchmove", handleEventMove);
+  projectNavContainer.addEventListener("touchend", handleEventEnd);
+
+  projectNavContainer.addEventListener("mousedown", handleEventStart);
+  projectNavContainer.addEventListener("mousemove", handleEventMove);
+  projectNavContainer.addEventListener("mouseleave", handleEventEnd);
+  projectNavContainer.addEventListener("mouseup", handleEventEnd);
+}
+
+// Project Menu Selection Functionality
 const projectButtons = document.querySelectorAll("[data-project-button]");
 
-projectNavContainer.addEventListener("touchstart", handleEventStart);
-projectNavContainer.addEventListener("touchmove", handleEventMove);
-projectNavContainer.addEventListener("touchend", handleEventEnd);
+if (projectButtons) {
+  let projectButtonsArray = Array.from(projectButtons);
+  projectButtonsArray[0].classList.add("active");
 
-projectNavContainer.addEventListener("mousedown", handleEventStart);
-projectNavContainer.addEventListener("mousemove", handleEventMove);
-projectNavContainer.addEventListener("mouseleave", handleEventEnd);
-projectNavContainer.addEventListener("mouseup", handleEventEnd);
-
-let projectButtonsArray = Array.from(projectButtons);
-projectButtonsArray.forEach((button) => {
-  button.addEventListener("click", () => {
-    button.classList.add("active");
-    projectButtonsArray.forEach((unSelectedButton) => {
-      if (unSelectedButton != button) {
-        unSelectedButton.classList.remove("active");
-      }
+  projectButtonsArray.forEach((button) => {
+    button.addEventListener("click", () => {
+      button.classList.add("active");
+      projectButtonsArray.forEach((unSelectedButton) => {
+        if (unSelectedButton != button) {
+          unSelectedButton.classList.remove("active");
+        }
+      });
     });
   });
+}
+
+// Page Flip Functionality
+const directionButton = document.querySelector("[data-direction-button]");
+const pageBoxInner = document.querySelector("[data-pagebox-inner]");
+const projectListContainer = document.querySelector("[data-project-list]");
+
+if (pageBoxInner) {
+  if (directionButton) {
+    directionButton.addEventListener("click", (e) => {
+      pageBoxInner.style.transform = "rotateX(180deg)";
+    });
+  }
+
+  if (projectListContainer) {
+    projectListContainer.addEventListener("scroll", (e) => {
+      if (projectListContainer.scrollTop === 0) {
+        pageBoxInner.style.transform = "rotateX(0deg)";
+      }
+    });
+  }
+}
+
+// Theme Switch Functionality
+const themeSwitchControl1 = document.querySelector(
+  "[data-projects-theme-switch1]"
+);
+
+const themeSwitchControl2 = document.querySelector(
+  "[data-projects-theme-switch2]"
+);
+
+themeSwitchControl1.checked = true;
+themeSwitchControl2.checked = true;
+
+themeSwitchControl1.addEventListener("change", (e) => {
+  if (e.currentTarget.checked == true) {
+    // bright theme here
+  } else {
+    // dark theme here
+  }
 });
+
+// if (themeSwitchControl) {
+//   themeSwitchControl.addEventListener("change", (e) => {
+//     if (e.currentTarget.checked == true) {
+//       console.log("checked");
+//     } else {
+//       console.log("not checked");
+//     }
+//   });
+// }
